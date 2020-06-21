80a1d45547b4aa1487c60e3cebae01383426fc63
james-daniels <james.daniels@hotmail.com>
2020-06-21 15:29:40 -0500

    first commit

---
+++ handler.go
@@ -0,0 +1,81 @@
+package urlshort
+
+import (
+	"net/http"
+	"gopkg.in/yaml.v3"
+	"encoding/json"
+)
+
+type pathURL struct {
+	Path string
+	URL string
+}
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request){
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
+	
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(ybyte []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathUrls []pathURL
+
+	err := yaml.Unmarshal(ybyte, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _,pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+//JSONHandler will parse the provided JSON and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+func JSONHandler(jbyte []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathUrls []pathURL
+
+	err := json.Unmarshal(jbyte, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	pathsToUrls := make(map[string]string)
+	for _,pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	
+	return MapHandler(pathsToUrls, fallback), nil
+}
---
+++ main/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/james-daniels/gophersizes/urlshort"
+)
+
+var yamlFile string
+var jsonFile string
+
+// var yaml = `
+// - path: /urlshort
+//   url: https://github.com/gophercises/urlshort
+// - path: /urlshort-final
+//   url: https://github.com/gophercises/urlshort/tree/solution
+// `
+
+// Build the MapHandler using the mux as the fallback
+var pathsToUrls = map[string]string{}
+
+func init(){
+	flag.StringVar(&yamlFile, "yaml", "", "read a yaml file?")
+	flag.StringVar(&jsonFile, "json", "", "read a json file?")
+}
+
+func main() {
+	flag.Parse()
+
+	mux := defaultMux()
+// Build the MapHandler using the mux as the fallback
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
+
+	if yamlFile == "" && jsonFile == "" {
+		fmt.Println("need to use either a yaml or json file")
+		os.Exit(0)
+	} else if yamlFile != "" && jsonFile != "" {
+		fmt.Println("there can only be one!")
+		os.Exit(0)
+	}
+
+	if jsonFile != "" {
+		execJSON(mapHandler)
+	} else if yamlFile != "" {
+		execYAML(mapHandler)
+	}
+
+}
+
+
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
+
+func parseFile(file string )[]byte {
+	fbyte, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	return fbyte
+}
+
+
+func execJSON(mapHandler http.HandlerFunc){
+	jsonInput := parseFile(jsonFile)
+	jsonHandler, err := urlshort.JSONHandler(jsonInput, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", jsonHandler)
+}
+
+func execYAML(mapHandler http.HandlerFunc){
+	yamlInput := parseFile(yamlFile)
+	yamlHandler, err := urlshort.YAMLHandler(yamlInput, mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
